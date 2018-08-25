66a829bed9c14fa843d2f4b461b2916baa498490
Emmanuele Abbenante <emmanuele.abbenante@gmail.com>
2018-08-26 00:07:31 +0200

    Description parser, handled withdrawals. Thrown exception for unknown descriptions.

--- src/main/java/com/emmanuele/transactionreport/TransactionDescriptionParser.java
+++ src/main/java/com/emmanuele/transactionreport/TransactionDescriptionParser.java
@@ -14,19 +14,24 @@ public class TransactionDescriptionParser {
 
 	private static final Pattern VPAY_OPERATION = Pattern
 			.compile("Operazione VPAY del (?<date>.*) alle ore (?<time>.*) con Carta[ \t\n]*(?<cardnumber>.*) Div=(?<currency>.*) Importo in divisa=(?<currencyamount>.*) \\/ Importo in Euro=(?<euramount>.*)[ \t\n]*presso (?<counterpart>.*) - Transazione C-less");
+	private static final Pattern WITHDRAWAL_OPERATION = Pattern
+			.compile("Prelievo carta del (?<date>.*) alle ore (?<time>.*) con Carta[ \t\n]*(?<cardnumber>.*) di Abi Div=(?<currency>.*) Importo in divisa=(?<currencyamount>.*) / Importo in[ \t\n]*Euro=(?<euramount>.*) presso (?<counterpart>.*)");
 
 	private TransactionDescriptionParser() {
 	}
 
 	public static Transaction parseDescription(final String description) {
-		final Transaction transaction = new Transaction();
 		final String cleanedDescriptionStr = description.replaceAll(
 				NOT_ALLOWED_CHARACTERS_REGEX, "");
-		final Matcher matcher = VPAY_OPERATION.matcher(cleanedDescriptionStr);
+		Matcher matcher = VPAY_OPERATION.matcher(cleanedDescriptionStr);
 		if (matcher.matches()) {
 			return parseVPayOperationDescription(matcher);
 		}
-		return transaction;
+		matcher = WITHDRAWAL_OPERATION.matcher(cleanedDescriptionStr);
+		if (matcher.matches()) {
+			return parseVPayOperationDescription(matcher);
+		}
+		throw new IllegalArgumentException("Unknown description: " + description);
 	}
 
 	private static Transaction parseVPayOperationDescription(
